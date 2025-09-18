99f25582cae1620c953ce7a8085acc5963915674
Rajibbepary <rajib635356@gmail.com>
2025-09-18 21:07:46 +0600

    feat(assets): added banners, brand logos, and icons with centralized export

---
+++ client/src/assets/assets.js
@@ -0,0 +1,64 @@
+import banner1 from './banner/banner1.png'
+import banner2 from './banner/banner2.png'
+import banner3 from './banner/banner3.png'
+import amazon_vector from './brands/amazon_vector.png'
+import amazon from './brands/amazon.png'
+import casio from './brands/casio.png'
+import moonstar from './brands/moonstar.png'
+import randstad from './brands/randstad.png'
+import start_people from './brands/start-people 1.png'
+import start from './brands/start.png'
+import agent from './agent-pending.png'
+import authImage from './authImage.png'
+import merchant from './be-a-merchant-bg.png'
+import deliveryman from './big-deliveryman.png'
+import bookingIcon from './bookingIcon.png'
+import brand from './brand.png'
+import customer from './customer-top.png'
+import delivery from './delivery-van.png'
+import download from './download.png'
+import icons from './icons8-running.gif'
+import imageupload from './image-upload-icon.png'
+import livetracking from './live-tracking.png'
+import location from './location-merchant.png'
+import logo from './logo.png'
+import review from './reviewQuote.png'
+import safe from './safe-delivery.png'
+import service from './service.png'
+import tiny from './tiny-deliveryman.png'
+import menuIcon from './menu_icon.svg'
+import closemenu from './close_icon.svg'
+const assets = {
+    banner1,
+    banner2,
+    banner3,
+    amazon_vector,
+    amazon,
+    casio,
+    moonstar,
+    randstad,
+    start_people,
+    start,
+    agent,
+    authImage,
+    merchant,
+    deliveryman,
+    bookingIcon,
+    brand,
+    customer,
+    delivery,
+    download,
+    icons,
+    imageupload,
+    livetracking,
+    location,
+    logo,
+    review,
+    safe,
+    service,
+    tiny,
+    menuIcon,
+    closemenu
+}
+
+export default assets
