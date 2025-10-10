0049ebfcdfe694d9e509e7c58820be0bdbf871a5
Rajibbepary <rajib635356@gmail.com>
2025-10-10 18:42:59 +0600

    top button few style change

--- client/src/components/TopButton.jsx
+++ client/src/components/TopButton.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import { FaArrowUp } from "react-icons/fa6";
 
 const TopButton = () => {
      const [visible, setVisible] = useState(false);
@@ -29,11 +29,11 @@ const TopButton = () => {
          <button
       onClick={scrollToTop}
       type="button"
-      className={`py-2 px-6 bg-[#CAEB66] rounded-sm shadow-lg fixed right-4 bottom-28 transition-all duration-300 hover:bg-[#caeb66cf] ${
+      className={`py-2 px-8 flex text-xl justify-center items-center gap-1 bg-[#CAEB66] rounded-sm shadow-lg z-30 backdrop-blur-xl fixed right-0 bottom-28 transition-all duration-300 hover:bg-[#caeb66cf] ${
         visible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10 pointer-events-none"
       }`}
     >
-      ↑ Top
+     <span className="text-xl"><FaArrowUp /></span> Top
     </button>
     );
 };
