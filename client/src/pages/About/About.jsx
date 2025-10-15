269cf6ec794bd636f0be7dbee9505f5f4f441215
Rajibbepary <rajib635356@gmail.com>
2025-10-15 18:48:14 +0600

    about page add

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -1,9 +1,16 @@
 
 const About = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+   <div className="w-[200px] h-[120px] bg-green-600 relative mx-auto mt-5 ">
+  <div className="w-[120px] h-[120px] absolute bg-red-700 rounded-full 
+                  left-0 right-0 top-0 bottom-0 m-auto ">
+  </div>
+</div>
+<div className="max-h-[150px] w-[500px] bg-green-400 overflow-scroll">
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, dignissimos laboriosam, natus animi nemo ullam nisi neque consequatur sunt dolores corrupti ducimus facere iure assumenda ab adipisci id! Ipsa itaque nam quibusdam modi officia expedita repudiandae, accusamus, ratione, labore nesciunt veniam? Ducimus cupiditate quo, sequi voluptatum sed quibusdam autem ab maiores odio in facilis molestiae dolore doloremque nam suscipit soluta possimus ipsam itaque assumenda ea fuga reiciendis debitis praesentium sapiente? Ea inventore aperiam aliquam, laborum totam maiores eum consectetur enim dolorem repellat sunt autem facere fugit odit. Suscipit, iusto provident? Porro eveniet qui ad expedita earum provident quaerat sunt esse.
+</div>
+</>
   );
 };
 
