7eff2aa7dccf12a7630db0e3a3edecaca63b9abf
Rajibbepary <rajib635356@gmail.com>
2025-10-17 22:37:22 +0600

    about page tab add

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -2,14 +2,21 @@
 const About = () => {
   return (
     <>
-   <div className="w-[200px] h-[120px] bg-green-600 relative mx-auto mt-5 ">
-  <div className="w-[120px] h-[120px] absolute bg-red-700 rounded-full 
-                  left-0 right-0 top-0 bottom-0 m-auto ">
-  </div>
-</div>
-<div className="max-h-[150px] w-[500px] bg-green-400 overflow-scroll">
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, dignissimos laboriosam, natus animi nemo ullam nisi neque consequatur sunt dolores corrupti ducimus facere iure assumenda ab adipisci id! Ipsa itaque nam quibusdam modi officia expedita repudiandae, accusamus, ratione, labore nesciunt veniam? Ducimus cupiditate quo, sequi voluptatum sed quibusdam autem ab maiores odio in facilis molestiae dolore doloremque nam suscipit soluta possimus ipsam itaque assumenda ea fuga reiciendis debitis praesentium sapiente? Ea inventore aperiam aliquam, laborum totam maiores eum consectetur enim dolorem repellat sunt autem facere fugit odit. Suscipit, iusto provident? Porro eveniet qui ad expedita earum provident quaerat sunt esse.
-</div>
+    <div className="flex space-x-2 bg-white p-1 border border-gray-500/50 rounded-md text-sm">
+            <div className="flex items-center">
+                <input type="radio" name="options" id="html" className="hidden peer" checked />
+                <label htmlFor="html" className="cursor-pointer rounded py-2 px-8 text-gray-500 transition-colors duration-200 peer-checked:bg-indigo-600 peer-checked:text-white">HTML</label>
+            </div>
+            <div className="flex items-center">
+                <input type="radio" name="options" id="css" className="hidden peer" />
+                <label htmlFor="css" className="cursor-pointer rounded py-2 px-8 text-gray-500 transition-colors duration-200 peer-checked:bg-indigo-600 peer-checked:text-white">CSS</label>
+            </div>
+            <div className="flex items-center">
+                <input type="radio" name="options" id="react" className="hidden peer" />
+                <label htmlFor="react" className="cursor-pointer rounded py-2 px-8 text-gray-500 transition-colors duration-200 peer-checked:bg-indigo-600 peer-checked:text-white">React</label>
+            </div>
+        </div>
+
 </>
   );
 };
